c1f32b2d485bdc094b8558fb24d66d9817e62890
AbnerJosue <josueestrada688@gmail.com>
2024-08-19 17:29:22 -0600

    Instancias y Operaciones

--- src/basesjava/BasesJava.java
+++ src/basesjava/BasesJava.java
@@ -3,25 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package basesjava;
-
+import basesjava.Ejercicio;
 /**
  *
  * @author josue
  */
 public class BasesJava {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-         int numero =  20;
+        // Tipos de variables
+        int numero =  20;
         String nombre = "Libra";
         char letra = 'a';
         double temperatura = 20.30;
         Boolean siONo = false;
         long largo = 1234324;
-
 
         System.out.println(numero);
         System.out.println(nombre);
@@ -29,6 +25,42 @@ public class BasesJava {
         System.out.println(temperatura);
         System.out.println(siONo);        
         System.out.println(largo);
+        
+        // Tipos de Numericos
+           
+        int num1, num2, resultado;
+        
+        num1 = 5;
+        
+        num2 = 10;
+        
+        resultado = num1 + num2;
+        
+        System.out.println("El resultado es " + resultado );
+        
+        
+        // CONDICIONALES
+        
+        if( num1 > num2 ){
+            System.out.println(" Num1 es mayor num2");
+        } else {
+            System.out.println("No es");
+        }
+        
+        
+        Ejercicio ejercicio = new Ejercicio();
+
+        // Llamando al método saludar
+        ejercicio.saludar();
+
+        // Llamando al método sumar
+        int resultado2 = ejercicio.sumar(5, 7);
+        System.out.println("Resultado de la suma: " + resultado2);
+
+        // Llamando al método obtenerSaludo
+        String saludo = ejercicio.obtenerSaludo("Josue");
+        System.out.println(saludo); 
+        
         
     }
     
---
+++ src/basesjava/Ejercicio.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package basesjava;
+
+/**
+ *
+ * @author josue
+ */
+public class Ejercicio {
+    
+    
+    public void saludar() {
+        System.out.println("Hola, bienvenido!");
+    }
+
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public String obtenerSaludo(String nombre) {
+        return "Hola, " + nombre + "!";
+    }
+    
+}
