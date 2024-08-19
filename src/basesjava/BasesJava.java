46f1e22dcff9e98a3635397d03e589cfdf558325
AbnerJosue <josueestrada688@gmail.com>
2024-08-18 21:14:27 -0600

    Bases Java

---
+++ src/basesjava/BasesJava.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package basesjava;
+
+/**
+ *
+ * @author josue
+ */
+public class BasesJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int numero =  20;
+        String nombre = "Libra";
+        char letra = 'a';
+        double temperatura = 20.30;
+        Boolean siONo = false;
+        long largo = 1234324;
+
+
+        System.out.println(numero);
+        System.out.println(nombre);
+        System.out.println(letra);
+        System.out.println(temperatura);
+        System.out.println(siONo);        
+        System.out.println(largo);
+        
+    }
+    
+}
