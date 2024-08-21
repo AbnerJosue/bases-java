7bc148f58a3808ab3f145b2c80aca380636f7a4e
AbnerJosue <josueestrada688@gmail.com>
2024-08-21 16:06:24 -0600

    new changes

---
+++ src/basesjava/Ejercicio_Practico/Ejercicio_despensa.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package basesjava.Ejercicio_Practico;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author josue
+ */
+public class Ejercicio_despensa {
+    
+        public void despensa(){
+            
+           double sueldo;
+           int categoria;
+           
+           System.out.println(" Ingrese el tipo de categoria que desea calcular el sueldo ");
+           
+           Scanner teclado = new Scanner (System.in);
+           
+           categoria = teclado.nextInt();
+           
+           if ( categoria == 1 ) {
+            sueldo = 15890 + (15890 * 0.10);
+           } else if ( categoria == 2) {
+            sueldo = 25630.89;
+           } else if ( categoria == 3) {
+            sueldo = 35560.20 + (35560.20 * 0.11);
+           } else {
+               System.out.println("Debe ingresar un numero de categoria valida");
+           }
+           
+            
+           System.out.println("El sueldo de :" + categoria + "Es de :" + sueldo );
+           
+        }
+    
+}
