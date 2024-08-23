46509bdff959c151e20cd560bc387c690981b1ce
Josue Estrada <64668192+AbnerJosue@users.noreply.github.com>
2024-08-23 17:18:44 +0000

    uncomment

--- src/basesjava/BasesJava.java
+++ src/basesjava/BasesJava.java
@@ -15,58 +15,58 @@ public class BasesJava {
 
     public static void main(String[] args) {
 
-        // // Declaracion de Instancias
-        // // TIpoVariables variables = new TIpoVariables();
+        // Declaracion de Instancias
+        // TIpoVariables variables = new TIpoVariables();
         
-        // // Ejercicio1 ejercicio1 = new Ejercicio1();
-        // // EjercicioSwitch ejercicioSwitch = new EjercicioSwitch();
+        // Ejercicio1 ejercicio1 = new Ejercicio1();
+        // EjercicioSwitch ejercicioSwitch = new EjercicioSwitch();
 
-        // // variables.variables();
-        // // ejercicio1.numero();
-        // // ejercicio.saludar();
-        // // ejercicioSwitch.StructureOfSwitch();
+        // variables.variables();
+        // ejercicio1.numero();
+        // ejercicio.saludar();
+        // ejercicioSwitch.StructureOfSwitch();
 
 
-        // // Llamando al método obtenerSaludo
-        // Ejercicio ejercicio = new Ejercicio();
+        // Llamando al método obtenerSaludo
+        Ejercicio ejercicio = new Ejercicio();
        
-        // String saludo = ejercicio.obtenerSaludo("Josue");
-        // System.out.println(saludo);
+        String saludo = ejercicio.obtenerSaludo("Josue");
+        System.out.println(saludo);
 
-        // // // Llamando al método sumar
-        // int resultado2 = ejercicio.sumar(5, 7);
-        // System.out.println("Resultado de la suma: " + resultado2);
+        // // Llamando al método sumar
+        int resultado2 = ejercicio.sumar(5, 7);
+        System.out.println("Resultado de la suma: " + resultado2);
 
-        // // // desde otra carpeta
+        // // desde otra carpeta
 
-        // Ejercicio_despensa ejercicio_despensa = new Ejercicio_despensa();
-        // ejercicio_despensa.despensa();
+        Ejercicio_despensa ejercicio_despensa = new Ejercicio_despensa();
+        ejercicio_despensa.despensa();
 
-        // // operador Ternario
+        // operador Ternario
 
-        // TernarioOperador ternario = new TernarioOperador(); 
-        // ternario.operador();
-
-
-        // // While 
-
-        // int contador = 0;
-
-        // while( contador <= 10) {
-
-        //     System.out.println("Estoy en la vuelta: "+ contador);
-        //     contador++;
-        // }
+        TernarioOperador ternario = new TernarioOperador(); 
+        ternario.operador();
 
 
-        // // While infinitos con estructuras repetitivas
+        // While 
 
-        // boolean centinela = true;
+        int contador = 0;
 
-        // while( centinela == true) {
-        //     System.out.println("Estoy en la vuelta: "+ centinela);
-        //     centinela = false;
-        // }
+        while( contador <= 10) {
+
+            System.out.println("Estoy en la vuelta: "+ contador);
+            contador++;
+        }
+
+
+        // While infinitos con estructuras repetitivas
+
+        boolean centinela = true;
+
+        while( centinela == true) {
+            System.out.println("Estoy en la vuelta: "+ centinela);
+            centinela = false;
+        }
 
         // Ciclo For 
 
