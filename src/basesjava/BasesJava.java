5e1d7f0d2bc98c7ab62029a6fab3e26f3fd0ef10
Josue Estrada <64668192+AbnerJosue@users.noreply.github.com>
2024-08-27 01:40:19 +0000

    Ejercicio 1 vectores

--- src/basesjava/BasesJava.java
+++ src/basesjava/BasesJava.java
@@ -5,7 +5,9 @@
 package basesjava;
 
 import basesjava.EjercicioDespensa.Ejercicio_despensa;
+import basesjava.EjerciciosVectoresMatrices.*;
 import basesjava.OperadorTernario.*;
+
 /**
  *
  * @author josue
@@ -16,7 +18,7 @@ public class BasesJava {
 
         // // Declaracion de Instancias
         // // TIpoVariables variables = new TIpoVariables();
-        
+
         // // Ejercicio1 ejercicio1 = new Ejercicio1();
         // // EjercicioSwitch ejercicioSwitch = new EjercicioSwitch();
 
@@ -25,10 +27,9 @@ public class BasesJava {
         // // ejercicio.saludar();
         // // ejercicioSwitch.StructureOfSwitch();
 
-
         // // Llamando al método obtenerSaludo
         MetodosEjemplos ejercicio = new MetodosEjemplos();
-       
+
         String saludo = ejercicio.obtenerSaludo("Josue");
         System.out.println(saludo);
 
@@ -43,67 +44,69 @@ public class BasesJava {
 
         // operador Ternario
 
-        TernarioOperador ternario = new TernarioOperador(); 
+        TernarioOperador ternario = new TernarioOperador();
         ternario.operador();
 
-
-        // While 
+        // While
 
         int contador = 0;
 
         while( contador <= 10) {
 
-            System.out.println("Estoy en la vuelta: "+ contador);
-            contador++;
+        System.out.println("Estoy en la vuelta: "+ contador);
+        contador++;
         }
-
 
         // While infinitos con estructuras repetitivas
 
         boolean centinela = true;
 
         while( centinela == true) {
-            System.out.println("Estoy en la vuelta: "+ centinela);
-            centinela = false;
+        System.out.println("Estoy en la vuelta: "+ centinela);
+        centinela = false;
         }
 
-        // Ciclo For 
+        // Ciclo For
 
         for( int cont = 0; cont <= 10; cont++){
 
-            System.out.println("El valor es el siguiente " + cont);
+        System.out.println("El valor es el siguiente " + cont);
 
         }
 
-
-        // Declararion de Vectores 
+        // Declararion de Vectores
 
         int vector [] = new int [4];
         // int vector []; sin asignar el tamaño
 
         // Asignaciond ( manual )
-        vector[0] = 2; 
-        vector[1] = 50; 
-        vector[2] = 1223; 
-        vector[3] = 44; 
-        // vector[4] = 44;  Esto genera un error ya que no es del mismo tamaño Index 4 out of bounds for length 4
+        vector[0] = 2;
+        vector[1] = 50;
+        vector[2] = 1223;
+        vector[3] = 44;
+        
+        /* 
+            vector[4] = 44; 
+            (Esto genera un error ya que no es del mismo tamaño Index 4)
+            out of bounds for length 4
+        */ 
 
         System.out.println(vector);
 
-        // Recorrido 
+        // Recorrido
 
         for( int i = 0; i < vector.length; i ++ ){
-            System.out.println(vector[i]);
+        System.out.println(vector[i]);
         }
 
-        // Matriz 
+        // Matriz
 
-        int matriz [][] = new int [3][3];   // 3 filas , 3 columnas
-        
+        int matriz [][] = new int [3][3]; // 3 filas , 3 columnas
+
         // matriz = [
-        //         [0, 0, 0],  # Fila 0
-        //         [0, 0, 0],  # Fila 1
-        //         [0, 0, 0]   # Fila 2
+        // [0, 0, 0], # Fila 0
+        // [0, 0, 0], # Fila 1
+        // [0, 0, 0] # Fila 2
         // ]
 
         // Asignacion Manual
@@ -113,13 +116,14 @@ public class BasesJava {
         matriz[1][1] = 1900;
         matriz[1][2] = 50;
 
-
         for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                System.out.print(matriz[i][j] + " ");
-            }
-            System.out.println();
+        for (int j = 0; j < matriz[i].length; j++) {
+        System.out.print(matriz[i][j] + " ");
         }
-    }
+        System.out.println();
+        }
 
+        Ejercicio1 findTheNumberEjercicio1 = new Ejercicio1();
+        findTheNumberEjercicio1.manyTimesRepited();
+    }
 }
